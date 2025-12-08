0089a8c96cd40b94d914c7e996010bcdfb0044b6
SW-HyunjunSon <hyunjun6133@naver.com>
2025-12-08 16:20:07 +0900

    fix: API url 작성 방식 수정

--- app/src/main/java/com/arttrip/android/data/remote/api/HomeApi.kt
+++ app/src/main/java/com/arttrip/android/data/remote/api/HomeApi.kt
@@ -1,15 +1,16 @@
 package com.arttrip.android.data.remote.api
 
+import com.arttrip.android.data.remote.api.ApiConstants.HOME_PATH
 import com.arttrip.android.data.remote.model.home.ExhibitDto
 import com.arttrip.android.data.remote.model.network.BaseResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface HomeApi {
-    @GET(ApiConstants.HOME_INTER_PATH)
+    @GET("${HOME_PATH}/overseas")
     suspend fun getCountryList(): BaseResponseDto<List<String>>
 
-    @GET(ApiConstants.HOME_RECOMMEND_TODAY_PATH)
+    @GET("${HOME_PATH}/recommend/today")
     suspend fun getHomeRecommendToday(
         @Query("isDomestic") isDomestic: Boolean
     ): BaseResponseDto<List<ExhibitDto>>
