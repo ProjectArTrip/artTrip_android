b8df44e89056312692329204c752e3ea4d14c8cc
Seulah-Lim <90150969+Seul-Seul@users.noreply.github.com>
2025-12-10 15:34:17 +0900

    chore: 변경된 홈 네비게이션 예시 주석 추가

--- app/src/main/java/com/arttrip/android/presentation/home/HomeRoute.kt
+++ app/src/main/java/com/arttrip/android/presentation/home/HomeRoute.kt
@@ -9,10 +9,24 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 @Composable
 fun HomeRoute(
     innerPadding: PaddingValues,
+    onNavigate: (String) -> Unit,
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
     val homeState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    //이동 예시
+//    LaunchedEffect(Unit) {
+//        viewModel.effect.collect { effect ->
+//            when (effect) {
+//                HomeEffect.NavigateToDateFilter -> {
+//                    onNavigate(AppRoute.HOME_DATE_FILTER)
+//                }
+//                is HomeEffect.ShowToast -> {
+//                    // 토스트, 스낵바 같은 UI 사이드 이펙트 처리
+//                }
+//            }
+//        }
+//    }
     HomeScreen(
         innerPadding = innerPadding,
         uiState = homeState,
