c82587e6153eff8932d43daf80d08cfed71c712a
Seulah-Lim <90150969+Seul-Seul@users.noreply.github.com>
2025-12-16 16:04:05 +0900

    chore: lint 적용

--- app/src/main/java/com/arttrip/android/presentation/exhibition/contract/ExhibitionDetailEffect.kt
+++ app/src/main/java/com/arttrip/android/presentation/exhibition/contract/ExhibitionDetailEffect.kt
@@ -1,9 +1,8 @@
 package com.arttrip.android.presentation.exhibition.contract
 
-import com.arttrip.android.presentation.intro.contract.IntroEffect
-
 sealed interface ExhibitionDetailEffect {
     data object NavigateBack : ExhibitionDetailEffect
+
     data class ShowError(
         val message: String,
     ) : ExhibitionDetailEffect
