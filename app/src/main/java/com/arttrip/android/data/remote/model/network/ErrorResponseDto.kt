af25ef01343d36a8d182b5fd28d1b3a67aeabb14
Seul-Seul <90150969+Seul-Seul@users.noreply.github.com>
2025-12-30 02:30:25 +0900

    fix: ErrorResponse에 JsonIgnoreUnknownKeys 추가

--- app/src/main/java/com/arttrip/android/data/remote/model/network/ErrorResponseDto.kt
+++ app/src/main/java/com/arttrip/android/data/remote/model/network/ErrorResponseDto.kt
@@ -1,8 +1,12 @@
 package com.arttrip.android.data.remote.model.network
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonIgnoreUnknownKeys
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
+@JsonIgnoreUnknownKeys
 data class ErrorResponseDto(
     val isSuccess: Boolean?,
     val code: String?,
