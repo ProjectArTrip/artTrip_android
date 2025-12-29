3e6270cdf6aa6435ab697ad915349e9fef1c9fae
kznHJ <hyunjun6133@naver.com>
2025-12-30 02:41:56 +0900

    fix: 빈 데이터 UI 수정

--- app/src/main/java/com/arttrip/android/presentation/home/ui/EmptyData.kt
+++ app/src/main/java/com/arttrip/android/presentation/home/ui/EmptyData.kt
@@ -14,12 +14,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.arttrip.android.R
 import com.arttrip.android.core.model.enums.exhibition.ExhibitionGenre
 import com.arttrip.android.core.ui.theme.AppColor
 import com.arttrip.android.core.ui.theme.AppTextStyle
+
+@Composable
+fun EmptyPersonalizedExhibition() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = AppColor.SubLightGray)
+            .padding(top = 24.dp, bottom = 28.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_empty_data_40),
+                contentDescription = "No Data",
+                tint = Color.Unspecified
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(8.dp)
+            )
+            Text(
+                text = "회원님을 위한 전시 추천이 존재하지 않습니다.",
+                style = AppTextStyle.Body01Regular,
+                color = AppColor.TextTertiary
+            )
+        }
+    }
+}
 
 @Composable
 fun EmptyScheduleExhibition() {
@@ -36,8 +69,9 @@ fun EmptyScheduleExhibition() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Icon(
-                painter = painterResource(R.drawable.ic_no_data_40),
-                contentDescription = "No Data"
+                painter = painterResource(R.drawable.ic_empty_data_40),
+                contentDescription = "No Data",
+                tint = Color.Unspecified
             )
             Spacer(
                 modifier = Modifier
@@ -67,8 +101,9 @@ fun EmptyGenreExhibition(genre: ExhibitionGenre) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Icon(
-                painter = painterResource(R.drawable.ic_no_data_40),
-                contentDescription = "No Data"
+                painter = painterResource(R.drawable.ic_empty_data_40),
+                contentDescription = "No Data",
+                tint = Color.Unspecified
             )
             Spacer(
                 modifier = Modifier
