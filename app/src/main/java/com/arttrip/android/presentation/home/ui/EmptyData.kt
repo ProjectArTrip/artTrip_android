e00cace7a8a637e50c7df06ad8e335e685bc7188
kznHJ <hyunjun6133@naver.com>
2025-12-29 23:34:26 +0900

    feat: Home Screen 빈 리스트 UI 추가

---
+++ app/src/main/java/com/arttrip/android/presentation/home/ui/EmptyData.kt
@@ -0,0 +1,84 @@
+package com.arttrip.android.presentation.home.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.arttrip.android.R
+import com.arttrip.android.core.model.enums.exhibition.ExhibitionGenre
+import com.arttrip.android.core.ui.theme.AppColor
+import com.arttrip.android.core.ui.theme.AppTextStyle
+
+@Composable
+fun EmptyScheduleExhibition() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = AppColor.SubLightGray)
+            .padding(top = 24.dp, bottom = 28.dp)
+            .clip(RoundedCornerShape(8.dp))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_no_data_40),
+                contentDescription = "No Data"
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(8.dp)
+            )
+            Text(
+                text = "해당 날짜에 진행중인 전시가 존재하지 않습니다.",
+                style = AppTextStyle.Body01Regular,
+                color = AppColor.TextTertiary
+            )
+        }
+    }
+}
+
+@Composable
+fun EmptyGenreExhibition(genre: ExhibitionGenre) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = AppColor.SubLightGray)
+            .padding(top = 24.dp, bottom = 28.dp)
+            .clip(RoundedCornerShape(8.dp))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_no_data_40),
+                contentDescription = "No Data"
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(8.dp)
+            )
+            Text(
+                text = "${genre.label} 장르의 전시가 존재하지 않습니다.",
+                style = AppTextStyle.Body01Regular,
+                color = AppColor.TextTertiary
+            )
+        }
+    }
+}
