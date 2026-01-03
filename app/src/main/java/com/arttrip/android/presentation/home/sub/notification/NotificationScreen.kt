a7a150cf464e87daa56a8d1276093a87a4e97d1b
kznHJ <hyunjun6133@naver.com>
2026-01-04 01:08:16 +0900

    feat: 알림 화면 UI 구현

--- app/src/main/java/com/arttrip/android/presentation/home/sub/notification/NotificationScreen.kt
+++ app/src/main/java/com/arttrip/android/presentation/home/sub/notification/NotificationScreen.kt
@@ -1,6 +1,6 @@
 package com.arttrip.android.presentation.home.sub.notification
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,8 +20,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.arttrip.android.R
+import com.arttrip.android.core.ui.component.appbar.AppTopBar
+import com.arttrip.android.core.ui.component.button.AppIconButton
 import com.arttrip.android.core.ui.theme.AppColor
 import com.arttrip.android.core.ui.theme.AppTextStyle
 
@@ -30,7 +35,6 @@ fun NotificationScreen(innerPadding: PaddingValues) {
             Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .background(color = Color.Gray),
     ) {
         Column(
             modifier = Modifier
@@ -40,21 +44,54 @@ fun NotificationScreen(innerPadding: PaddingValues) {
                 modifier = Modifier
                     .height(16.dp)
             )
-            Box(
+            AppTopBar(
+                title = "알림",
+                leading = {
+                    AppIconButton(
+                        iconResId = R.drawable.ic_back_24,
+                        contentDescription = "Back Button",
+                        onIconClick = {
+
+                        }
+                    )
+                }
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(12.dp)
+            )
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(52.dp)
-                    .background(color = Color.Blue),
-                contentAlignment = Alignment.Center
+                    .padding(horizontal = 24.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
             ) {
-                Text("앱바 영역")
+                repeat(10) {
+                    NotificationItem(
+                        isRead = false,
+                        title = "알림 타이틀",
+                        message = "알림 내용"
+                    )
+                }
+                repeat(10) {
+                    NotificationItem(
+                        isRead = true,
+                        title = "알림 타이틀",
+                        message = "알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 "
+                    )
+                }
+                Spacer(
+                    modifier = Modifier
+                        .height(28.dp)
+                )
             }
         }
     }
 }
 
 @Composable
-fun NotificationItem(isRead: Boolean) {
+fun NotificationItem(isRead: Boolean, title: String, message: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -64,9 +101,14 @@ fun NotificationItem(isRead: Boolean) {
                 .height(12.dp)
         )
         Row {
+            Spacer(
+                modifier = Modifier
+                    .width(12.dp)
+            )
             Icon(
                 painter = painterResource(if (isRead) R.drawable.ic_alert_24 else R.drawable.ic_alert_badge_24),
-                contentDescription = "Alert Icon"
+                contentDescription = "Alert Icon",
+                tint = Color.Unspecified
             )
             Spacer(
                 modifier = Modifier
@@ -77,16 +119,41 @@ fun NotificationItem(isRead: Boolean) {
                     modifier = Modifier
                         .height(1.dp)
                 )
-                Text(
-                    text = "알림 타이틀",
-                    style = AppTextStyle.Title02Bold,
-                    color = AppColor.TextPrimary
-                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = title,
+                        style = AppTextStyle.Title02Bold,
+                        color = AppColor.TextPrimary
+                    )
+                    Text(
+                        text = "N분 전",
+                        style = AppTextStyle.Body02Light,
+                        color = AppColor.TextTertiary
+                    )
+                }
                 Spacer(
                     modifier = Modifier
                         .height(8.dp)
                 )
-                Text("")
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .height(40.dp),
+                        text = message,
+                        style = AppTextStyle.Body01Regular,
+                        color = AppColor.TextSecondary,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis)
+                }
             }
         }
     }
