4f62732c6134b4f2fa669c0e22f72760b28feb89
kznHJ <hyunjun6133@naver.com>
2026-01-04 03:28:49 +0900

    feat: 국내 지역별 전시 리스트 MVI 구조 추가

---
+++ app/src/main/java/com/arttrip/android/presentation/home/sub/region/RegionRoute.kt
@@ -0,0 +1,15 @@
+package com.arttrip.android.presentation.home.sub.region
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+
+@Composable
+fun RegionRoute(
+    innerPadding: PaddingValues,
+    viewModel: RegionViewModel = hiltViewModel(),
+) {
+//    RegionScreen(
+//        innerPadding = innerPadding,
+//    )
+}
---
+++ app/src/main/java/com/arttrip/android/presentation/home/sub/region/contract/RegionEffect.kt
@@ -0,0 +1,3 @@
+package com.arttrip.android.presentation.home.sub.region.contract
+
+class RegionEffect
