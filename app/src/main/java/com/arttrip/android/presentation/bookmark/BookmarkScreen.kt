c4c05e0d8ff6e41521a1016b5f7a0e453dcafbcf
Seul-Seul <90150969+Seul-Seul@users.noreply.github.com>
2026-01-04 04:46:27 +0900

    style: 즐겨찾기 화면에 AppBar 추가

--- app/src/main/java/com/arttrip/android/presentation/bookmark/BookmarkScreen.kt
+++ app/src/main/java/com/arttrip/android/presentation/bookmark/BookmarkScreen.kt
@@ -3,9 +3,11 @@ package com.arttrip.android.presentation.bookmark
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.arttrip.android.R
+import com.arttrip.android.core.ui.component.appbar.AppTopBar
+import com.arttrip.android.core.ui.component.button.AppIconButton
 import com.arttrip.android.presentation.bookmark.contract.BookmarkIntent
 import com.arttrip.android.presentation.bookmark.contract.BookmarkState
 
@@ -18,8 +20,14 @@ fun BookmarkScreen(
     Column(
         modifier = Modifier.padding(innerPadding),
     ) {
-        Text(
-            text = "Bookmark",
+        AppTopBar(
+            title = "즐겨찾기",
+            actions = {
+                AppIconButton(
+                    iconResId = R.drawable.ic_alert_24,
+                    onIconClick = {},
+                )
+            },
         )
     }
 }
