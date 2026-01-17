0e1b6f895d66d8d3b99a165aea8f578dedc08d48
kznHJ <hyunjun6133@naver.com>
2026-01-18 06:03:38 +0900

    feat: 장르별 리스트 화면 UI 구현

--- app/src/main/java/com/arttrip/android/presentation/home/sub/genre/GenreScreen.kt
+++ app/src/main/java/com/arttrip/android/presentation/home/sub/genre/GenreScreen.kt
@@ -9,28 +9,31 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.arttrip.android.R
 import com.arttrip.android.core.model.enums.exhibition.ExhibitionGenre
+import com.arttrip.android.core.model.enums.exhibition.ExhibitionStatus
 import com.arttrip.android.core.model.enums.foreign.ForeignCountry
 import com.arttrip.android.core.ui.component.appbar.AppTopBar
 import com.arttrip.android.core.ui.component.button.AppIconButton
+import com.arttrip.android.core.ui.component.button.LikeButton
+import com.arttrip.android.core.ui.component.tag.AppTag
 import com.arttrip.android.core.ui.theme.AppColor
 import com.arttrip.android.core.ui.theme.AppTextStyle
+import com.arttrip.android.core.util.noRippleClickable
+import com.arttrip.android.domain.model.exhibition.Exhibition
+import com.arttrip.android.presentation.home.ExhibitionImage
+import com.arttrip.android.presentation.home.ExhibitionImageCase
 import com.arttrip.android.presentation.home.sub.genre.contract.GenreIntent
-import com.arttrip.android.presentation.home.sub.notification.contract.NotificationIntent
 
 @Composable
 fun GenreScreen(innerPadding: PaddingValues, onIntent: (GenreIntent) -> Unit, country: ForeignCountry?, genre: ExhibitionGenre) {
@@ -51,7 +54,7 @@ fun GenreScreen(innerPadding: PaddingValues, onIntent: (GenreIntent) -> Unit, co
                         .height(16.dp),
             )
             AppTopBar(
-                title = "나라 : ${country?.label ?: "없음"}, genre : ${genre.label} ",
+                title = "${genre.label} 전시 추천",
                 leading = {
                     AppIconButton(
                         iconResId = R.drawable.ic_back_24,
@@ -61,123 +64,166 @@ fun GenreScreen(innerPadding: PaddingValues, onIntent: (GenreIntent) -> Unit, co
                         },
                     )
                 },
+                actions = {
+                    AppIconButton(
+                        iconResId = R.drawable.ic_alert_badge_24,
+                        contentDescription = "Notification Button",
+                        onIconClick = {
+
+                        }
+                    )
+                }
+
             )
-            Spacer(
-                modifier =
-                    Modifier
-                        .height(12.dp),
-            )
-            Column(
-                modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 24.dp)
-                        .verticalScroll(rememberScrollState()),
-                verticalArrangement = Arrangement.spacedBy(4.dp),
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp, vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                repeat(10) {
-                    NotificationItem(
-                        isRead = false,
-                        title = "알림 타이틀",
-                        message = "알림 내용",
-                    )
-                }
-                repeat(10) {
-                    NotificationItem(
-                        isRead = true,
-                        title = "알림 타이틀",
-                        message = "알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 알림 내용 ",
-                    )
-                }
-                Spacer(
-                    modifier =
-                        Modifier
-                            .height(28.dp),
+                Text(
+                    text = "총 N개",
+                    style = AppTextStyle.Title02Bold,
+                    color = AppColor.TextPrimary
+                )
+                AppIconButton(
+                    iconResId = R.drawable.ic_filter_24,
+                    contentDescription = "Filter Button",
+                    onIconClick = {}
                 )
             }
+            ExhibitionList(
+                onExhibitionClick = {},
+                onLikeClick = {}
+            )
         }
     }
 }
 
 @Composable
-fun NotificationItem(
-    isRead: Boolean,
-    title: String,
-    message: String,
+fun ExhibitionList(
+    onExhibitionClick: (Int) -> Unit,
+    onLikeClick: (Int) -> Unit,
 ) {
-    Column(
-        modifier =
-            Modifier
-                .fillMaxWidth(),
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        Spacer(
+        Column(
             modifier =
                 Modifier
-                    .height(12.dp),
-        )
-        Row {
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+                    .verticalScroll(rememberScrollState()),
+        ) {
+            val dummyExhibition =
+                Exhibition(
+                    id = 1,
+                    title = "DDP 매거진 라이브러리: 기록에 머물다",
+                    posterUrl = "https://i.pinimg.com/originals/5d/90/1f/5d901f30a1ee270123e19b1404165113.jpg",
+                    status = ExhibitionStatus.ONGOING,
+                    period = "2025.01.01 - 2025.12.31",
+                    hallName = "DDP 동대문디자인플라자",
+                    place = "서울 · 중구",
+                    isBookmarked = true,
+                )
             Spacer(
                 modifier =
                     Modifier
-                        .width(12.dp),
+                        .height(8.dp),
             )
-            Icon(
-                painter = painterResource(if (isRead) R.drawable.ic_alert_24 else R.drawable.ic_alert_badge_24),
-                contentDescription = "Alert Icon",
-                tint = Color.Unspecified,
-            )
-            Spacer(
-                modifier =
-                    Modifier
-                        .width(20.dp),
-            )
-            Column {
-                Spacer(
-                    modifier =
-                        Modifier
-                            .height(1.dp),
-                )
-                Row(
-                    modifier =
-                        Modifier
-                            .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                ) {
-                    Text(
-                        text = title,
-                        style = AppTextStyle.Title02Bold,
-                        color = AppColor.TextPrimary,
-                    )
-                    Text(
-                        text = "N분 전",
-                        style = AppTextStyle.Body02Light,
-                        color = AppColor.TextTertiary,
-                    )
-                }
-                Spacer(
-                    modifier =
-                        Modifier
-                            .height(8.dp),
-                )
-                Box(
-                    modifier =
-                        Modifier
-                            .fillMaxWidth()
-                            .height(40.dp),
-                ) {
-                    Text(
-                        modifier =
-                            Modifier
-                                .height(40.dp),
-                        text = message,
-                        style = AppTextStyle.Body01Regular,
-                        color = AppColor.TextSecondary,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis,
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+            ) {
+                repeat(10) {
+                    ExhibitionItem(
+                        exhibition = dummyExhibition,
+                        onExhibitionClick = { id ->
+                            onExhibitionClick(id)
+                        },
+                        onLikeClick = { id ->
+                            onLikeClick(id)
+                        }
                     )
                 }
             }
+            Spacer(
+                modifier =
+                    Modifier
+                        .height(24.dp),
+            )
+        }
+    }
+}
+
+@Composable
+fun ExhibitionItem(
+    exhibition: Exhibition,
+    onExhibitionClick: (Int) -> Unit,
+    onLikeClick: (Int) -> Unit,
+) {
+    Row(
+        modifier =
+            Modifier
+                .noRippleClickable {
+                    onExhibitionClick(exhibition.id)
+                },
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        ExhibitionImage(
+            url = exhibition.posterUrl,
+            case = ExhibitionImageCase.CASE3,
+        ) {
+            LikeButton(
+                modifier =
+                    Modifier
+                        .align(Alignment.TopEnd)
+                        .offset(x = (-8).dp, y = (8).dp),
+                isSelected = exhibition.isBookmarked,
+            ) {
+                onLikeClick(exhibition.id)
+            }
+            AppTag(
+                modifier =
+                    Modifier
+                        .align(Alignment.BottomEnd),
+                status = exhibition.status,
+            )
+        }
+        Spacer(
+            modifier =
+                Modifier
+                    .width(12.dp),
+        )
+        Column {
+            Text(
+                text = exhibition.title,
+                style = AppTextStyle.Body01Bold,
+                color = AppColor.TextPrimary,
+            )
+            Spacer(
+                modifier =
+                    Modifier
+                        .height(4.dp),
+            )
+            Text(
+                text = exhibition.hallName,
+                style = AppTextStyle.Body02Regular,
+                color = AppColor.TextTertiary,
+            )
+            Spacer(
+                modifier =
+                    Modifier
+                        .height(2.dp),
+            )
+            Text(
+                text = exhibition.period,
+                style = AppTextStyle.Body02Regular,
+                color = AppColor.TextTertiary,
+            )
         }
     }
 }
