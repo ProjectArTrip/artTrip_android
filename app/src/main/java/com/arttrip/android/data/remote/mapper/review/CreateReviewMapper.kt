6ccf330b380db1af38d3b1b22a80990c08541330
Seulah-Lim <90150969+Seul-Seul@users.noreply.github.com>
2026-03-10 15:25:23 +0900

    fix: 리뷰 작성 multipart 요청용 RequestBody 직렬화 추가

--- app/src/main/java/com/arttrip/android/data/remote/mapper/review/CreateReviewMapper.kt
+++ app/src/main/java/com/arttrip/android/data/remote/mapper/review/CreateReviewMapper.kt
@@ -1,12 +1,12 @@
 package com.arttrip.android.data.remote.mapper.review
 
 import com.arttrip.android.data.remote.model.review.CreateReviewReqDto
-import kotlinx.serialization.json.Json
+import com.google.gson.Gson
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 
 fun CreateReviewReqDto.toRequestBody(): RequestBody =
-    Json
-        .encodeToString(this)
+    Gson()
+        .toJson(this)
         .toRequestBody("application/json".toMediaType())
