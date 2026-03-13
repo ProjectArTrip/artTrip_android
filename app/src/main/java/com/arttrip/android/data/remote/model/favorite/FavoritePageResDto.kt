46b610458224c739a52509e0a5c5c0d3ebaa0e94
Seulah-Lim <90150969+Seul-Seul@users.noreply.github.com>
2026-03-13 17:15:27 +0900

    feat: 즐겨찾기 조회 API 관련 dataclass들 추가

---
+++ app/src/main/java/com/arttrip/android/data/remote/model/favorite/FavoritePageResDto.kt
@@ -0,0 +1,7 @@
+package com.arttrip.android.data.remote.model.favorite
+
+data class FavoritePageResDto<T>(
+    val favorites: List<T> = emptyList(),
+    val nextCursor: Int?,
+    val hasNext: Boolean,
+)
---
+++ app/src/main/java/com/arttrip/android/domain/model/favorite/Favorite.kt
@@ -0,0 +1,15 @@
+package com.arttrip.android.domain.model.favorite
+
+import com.arttrip.android.core.model.enums.exhibition.ExhibitionStatus
+
+data class Favorite(
+    val favoriteId: Int,
+    val exhibitId: Int,
+    val title: String,
+    val posterUrl: String,
+    val status: ExhibitionStatus,
+    val period: String,
+    val hallName: String,
+    val country: String?,
+    val region: String?,
+)
