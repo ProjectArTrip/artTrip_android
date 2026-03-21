7d71d91238f57536b98aac41a2f8b66db612484a
kznHJ <hyunjun6133@naver.com>
2026-03-22 01:02:22 +0900

    feat: 최근 검색어 API Domain Layer 생성

---
+++ app/src/main/java/com/arttrip/android/domain/repository/SearchHistoryRepository.kt
@@ -0,0 +1,11 @@
+package com.arttrip.android.domain.repository
+
+import com.arttrip.android.domain.model.network.ApiResult
+import com.arttrip.android.domain.model.recentsearch.RecentSearch
+import kotlinx.coroutines.flow.Flow
+
+interface SearchHistoryRepository {
+    fun getRecentSearchList(): Flow<ApiResult<List<RecentSearch>>>
+
+    fun deleteRecentSearch(id: Int): Flow<ApiResult<Unit>>
+}
