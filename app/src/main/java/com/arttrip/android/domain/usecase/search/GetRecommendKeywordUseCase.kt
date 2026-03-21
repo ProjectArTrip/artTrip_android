a4fc7882bf22983fa715a6fbecfa3e7aef857640
kznHJ <hyunjun6133@naver.com>
2026-03-22 01:28:32 +0900

    feat: 추천 검색어 Domain Layer 생성

---
+++ app/src/main/java/com/arttrip/android/domain/usecase/search/GetRecommendKeywordUseCase.kt
@@ -0,0 +1,16 @@
+package com.arttrip.android.domain.usecase.search
+
+import com.arttrip.android.domain.model.network.ApiResult
+import com.arttrip.android.domain.model.usertaste.Taste
+import com.arttrip.android.domain.repository.UserTasteRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetRecommendKeywordUseCase
+    @Inject
+    constructor(
+        private val userTasteRepository: UserTasteRepository,
+    ) {
+        operator fun invoke(): Flow<ApiResult<List<Taste>>> =
+            userTasteRepository.getRecommendKeywords()
+    }
