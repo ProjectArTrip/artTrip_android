ab4f137422dc4c8191e92ac83eb875276aa6e6dd
Seulah-Lim <90150969+Seul-Seul@users.noreply.github.com>
2026-04-27 15:10:39 +0900

    feat: notification 도메인 기반 FCM 토큰 등록 추가

--- app/src/main/java/com/arttrip/app/core/di/RepositoryModule.kt
+++ app/src/main/java/com/arttrip/app/core/di/RepositoryModule.kt
@@ -7,6 +7,7 @@ import com.arttrip.app.data.repository.ExhibitRepositoryImpl
 import com.arttrip.app.data.repository.HomeRepositoryImpl
 import com.arttrip.app.data.repository.MapRepositoryImpl
 import com.arttrip.app.data.repository.NoticeRepositoryImpl
+import com.arttrip.app.data.repository.NotificationRepositoryImpl
 import com.arttrip.app.data.repository.ProfileRepositoryImpl
 import com.arttrip.app.data.repository.ReviewRepositoryImpl
 import com.arttrip.app.data.repository.SearchHistoryRepositoryImpl
@@ -18,6 +19,7 @@ import com.arttrip.app.domain.repository.ExhibitRepository
 import com.arttrip.app.domain.repository.HomeRepository
 import com.arttrip.app.domain.repository.MapRepository
 import com.arttrip.app.domain.repository.NoticeRepository
+import com.arttrip.app.domain.repository.NotificationRepository
 import com.arttrip.app.domain.repository.ProfileRepository
 import com.arttrip.app.domain.repository.ReviewRepository
 import com.arttrip.app.domain.repository.SearchHistoryRepository
@@ -74,4 +76,8 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun curationRepository(curationRepositoryImpl: CurationRepositoryImpl): CurationRepository
+
+    @Singleton
+    @Binds
+    abstract fun notificationRepository(notificationRepositoryImpl: NotificationRepositoryImpl): NotificationRepository
 }
