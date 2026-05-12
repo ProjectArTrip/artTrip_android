549c10c406a2e2daecd090c31601fe871d308d77
Seulah-Lim <90150969+Seul-Seul@users.noreply.github.com>
2026-05-12 16:44:32 +0900

    chore: versionCode, versionName 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.arttrip.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
