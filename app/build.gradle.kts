f2a1641efc9fbacb986fc38cc5a6a31a5c5fca3d
SW-HyunjunSon <hyunjun6133@naver.com>
2026-05-15 13:36:35 +0900

    chore: versionCode, versionName 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.arttrip.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
